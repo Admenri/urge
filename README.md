c0ba6c43599df1c87a416b541f9482c69bfbfdbf
Admenri <admenri0504@gmail.com>
2026-06-06 11:10:01 +0800

    Update licence

---
+++ README.md
@@ -0,0 +1,15 @@
+﻿# ![Logo](app/resources/urge_favicon_64.png) Universal Ruby Game Engine (URGE) (WIP)
+
+## Preview
+
+ - URGE is a 3D-based game engine with ruby language supporting.  
+ - URGE provides compatitable scripts wrapper for supporting RPG Maker XP/VX/VX Ace.  
+ - URGE uses WebGPU specification as the rendering hardware interface backend, which supports modern graphics APIs like D3D12/Vulkan.  
+ - URGE can be used on popular platforms like Windows, Linux, Android and Emscripten.  
+
+## Contract
+
+- Mail: admenri@qq.com  
+- Site: https://admenri.com/  
+
+© 2018-2026 Admenri
---
+++ app/resources/resource.h
@@ -0,0 +1,27 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by app.rc
+//
+// Note: The //{{NO_DEPENDENCIES}} and //}}NO_DEPENDENCIES}} markers are used by Visual Studio
+// to identify blocks of symbols defined by the resource editor. Avoid manual edits
+// within these blocks if using the visual editor extensively.
+
+#pragma once // More modern alternative to traditional include guards for many compilers
+
+// Define resource IDs
+#define IDI_ICON1                       101 // Example: Main application icon ID
+
+// Next default values for new objects added via the resource editor
+// Only active when APSTUDIO_INVOKED is defined (i.e., within the VS resource editor)
+#ifdef APSTUDIO_INVOKED
+    #ifndef APSTUDIO_READONLY_SYMBOLS
+        // The resource editor will use these values as the starting point
+        // when assigning IDs to newly created resources of different types.
+        #define _APS_NEXT_RESOURCE_VALUE        102 // Next available value for general resources (dialogs, strings, etc.)
+        #define _APS_NEXT_COMMAND_VALUE         40001 // Next available value for command IDs (menus, accelerators)
+        #define _APS_NEXT_CONTROL_VALUE         1001 // Next available value for control IDs within dialogs
+        #define _APS_NEXT_SYMED_VALUE           101 // Next available value for symbol-only resources (can sometimes overlap initially)
+    #endif
+#endif
+
+//}}NO_DEPENDENCIES}} // End of the Visual Studio managed block
